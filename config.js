df9aeaf50eed7ff8abfda88b644f9326f503f3c2
vuduj <97089561+vuduj@users.noreply.github.com>
2023-02-07 14:31:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const nameGirl = 'Em';
+const nameGirl = 'mah pudding pie';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
+const eventName = 'happy our 1st valentine';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
